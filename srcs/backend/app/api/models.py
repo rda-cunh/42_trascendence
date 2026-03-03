1ca62349ad8e0f7d4202913dddadbfa048ea3690
rapcampo <rapcampo@student.42porto.com>
2026-03-03 02:29:33 +0000

    added changes to dockerfile to suport migrations for models and serializers. TODO: create POST and PATCH api_views

--- srcs/backend/app/api/models.py
+++ srcs/backend/app/api/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 
-class Product(models.model):
-    name = models.CharField(max_lenght=200)
+class Product(models.Model):
+    name = models.CharField(max_length=200)
     description = models.TextField(blank=True)     # optional description
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    currency = models.CharField(max_lenght=3, default="EUR")
+    currency = models.CharField(max_length=3, default="EUR")
 
     def __str__(self):
         return self.name
