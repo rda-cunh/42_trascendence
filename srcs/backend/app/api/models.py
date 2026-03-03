e9cb675fa7ea03e5b3b00219070bbbc6b9d0cc2a
rapcampo <rapcampo@student.42porto.com>
2026-03-03 02:16:47 +0000

    made placeholder models and serializer for easy to make new products with info provided, cannot yet be tested without proper database

--- srcs/backend/app/api/models.py
+++ srcs/backend/app/api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
+class Product(models.model):
+    name = models.CharField(max_lenght=200)
+    description = models.TextField(blank=True)     # optional description
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    currency = models.CharField(max_lenght=3, default="EUR")
+
+    def __str__(self):
+        return self.name
+
+
+'''
+class User(models.model):
+    name = models.CharField(max_length=30)
+
+'''
 # Create your models here.
--- srcs/backend/app/api/views.py
+++ srcs/backend/app/api/views.py
@@ -1,5 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+# from .models import Product
+# from .serializers import ProductSerializer
 
 
 @api_view(["GET"])
@@ -9,7 +11,8 @@ def listing_detail(request, id):
             "requested_id": id,
             "listing": {
                 "product_id": 1,
-                "title": "Infinity shader",
+                "name": "Infinity shader",
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fringilla mauris sed nibh scelerisque, ultrices ornare velit aliquam. Cras semper neque nec metus suscipit scelerisque. Nulla in lorem facilisis, efficitur libero nec, pellentesque eros. Aliquam at tortor odio. Nam pretium lobortis leo, vitae sodales eros sagittis fringilla. Sed ligula mauris, congue in finibus non, vulputate a neque. Nunc eleifend ex urna, id laoreet sapien aliquam eget. Donec lorem neque, efficitur et viverra eget, auctor sit amet nisl. Nulla sodales, nisl sit amet pharetra posuere, mi lorem cursus erat, sed ultricies quam urna a magna. Praesent tincidunt ligula in arcu condimentum lobortis. Nunc rhoncus lobortis nibh sit amet venenatis. Fusce eget cursus tellus. Proin molestie dolor eget nisl faucibus iaculis vel imperdiet tellus. Integer vel ligula sit amet lacus ornare facilisis vel et ipsum. Ut laoreet ipsum purus, sit amet posuere sapien eleifend et. In non felis a libero tempor vestibulum.",
                 "price": 19.99,
                 "currency": "EUR",
                 "status": "active",
@@ -18,34 +21,61 @@ def listing_detail(request, id):
     return Response(data)
 
 
+'''
+@api_view(["GET"])
+def listing_detail(request, id):
+
+    try:
+        product = Product.objects.get(pk=id)
+    except product.DoesNotExist:
+        return Response({"error": "Not Found."}, status=status.HTTP_404_NOT_FOUND)
+
+    data = ProductSerializer(product).data
+    return Response(data)
+'''
+
+'''
+@api_view(["GET"])
+def listing_full(request):
+
+    query = Product.objects.all().order_by("id")
+    data = ProductSerializer(query, many=true).data
+    return Responde({"count": len(data), "results": data})
+'''
+
+
 @api_view(["GET"])
 def listing_full(request):
 
     data = [
             {
                 "product_id": 1,
-                "title": "Infinity shader",
+                "name": "Infinity shader",
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fringilla mauris sed nibh scelerisque, ultrices ornare velit aliquam. Cras semper neque nec metus suscipit scelerisque. Nulla in lorem facilisis, efficitur libero nec, pellentesque eros. Aliquam at tortor odio. Nam pretium lobortis leo, vitae sodales eros sagittis fringilla. Sed ligula mauris, congue in finibus non, vulputate a neque. Nunc eleifend ex urna, id laoreet sapien aliquam eget. Donec lorem neque, efficitur et viverra eget, auctor sit amet nisl. Nulla sodales, nisl sit amet pharetra posuere, mi lorem cursus erat, sed ultricies quam urna a magna. Praesent tincidunt ligula in arcu condimentum lobortis. Nunc rhoncus lobortis nibh sit amet venenatis. Fusce eget cursus tellus. Proin molestie dolor eget nisl faucibus iaculis vel imperdiet tellus. Integer vel ligula sit amet lacus ornare facilisis vel et ipsum. Ut laoreet ipsum purus, sit amet posuere sapien eleifend et. In non felis a libero tempor vestibulum.",
                 "price": 19.99,
                 "currency": "EUR",
                 "status": "active",
                 },
             {
                 "product_id": 2,
-                "title": "Erik shader",
+                "name": "Erik shader",
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fringilla mauris sed nibh scelerisque, ultrices ornare velit aliquam. Cras semper neque nec metus suscipit scelerisque. Nulla in lorem facilisis, efficitur libero nec, pellentesque eros. Aliquam at tortor odio. Nam pretium lobortis leo, vitae sodales eros sagittis fringilla. Sed ligula mauris, congue in finibus non, vulputate a neque. Nunc eleifend ex urna, id laoreet sapien aliquam eget. Donec lorem neque, efficitur et viverra eget, auctor sit amet nisl. Nulla sodales, nisl sit amet pharetra posuere, mi lorem cursus erat, sed ultricies quam urna a magna. Praesent tincidunt ligula in arcu condimentum lobortis. Nunc rhoncus lobortis nibh sit amet venenatis. Fusce eget cursus tellus. Proin molestie dolor eget nisl faucibus iaculis vel imperdiet tellus. Integer vel ligula sit amet lacus ornare facilisis vel et ipsum. Ut laoreet ipsum purus, sit amet posuere sapien eleifend et. In non felis a libero tempor vestibulum.",
                 "price": 19.99,
                 "currency": "EUR",
                 "status": "active",
                 },
             {
                 "product_id": 3,
-                "title": "Rainbow shader",
+                "name": "Rainbow shader",
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fringilla mauris sed nibh scelerisque, ultrices ornare velit aliquam. Cras semper neque nec metus suscipit scelerisque. Nulla in lorem facilisis, efficitur libero nec, pellentesque eros. Aliquam at tortor odio. Nam pretium lobortis leo, vitae sodales eros sagittis fringilla. Sed ligula mauris, congue in finibus non, vulputate a neque. Nunc eleifend ex urna, id laoreet sapien aliquam eget. Donec lorem neque, efficitur et viverra eget, auctor sit amet nisl. Nulla sodales, nisl sit amet pharetra posuere, mi lorem cursus erat, sed ultricies quam urna a magna. Praesent tincidunt ligula in arcu condimentum lobortis. Nunc rhoncus lobortis nibh sit amet venenatis. Fusce eget cursus tellus. Proin molestie dolor eget nisl faucibus iaculis vel imperdiet tellus. Integer vel ligula sit amet lacus ornare facilisis vel et ipsum. Ut laoreet ipsum purus, sit amet posuere sapien eleifend et. In non felis a libero tempor vestibulum.",
                 "price": 19.99,
                 "currency": "EUR",
                 "status": "active",
                 },
             {
                 "product_id": 4,
-                "title": "Punk shader",
+                "name": "Punk shader",
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fringilla mauris sed nibh scelerisque, ultrices ornare velit aliquam. Cras semper neque nec metus suscipit scelerisque. Nulla in lorem facilisis, efficitur libero nec, pellentesque eros. Aliquam at tortor odio. Nam pretium lobortis leo, vitae sodales eros sagittis fringilla. Sed ligula mauris, congue in finibus non, vulputate a neque. Nunc eleifend ex urna, id laoreet sapien aliquam eget. Donec lorem neque, efficitur et viverra eget, auctor sit amet nisl. Nulla sodales, nisl sit amet pharetra posuere, mi lorem cursus erat, sed ultricies quam urna a magna. Praesent tincidunt ligula in arcu condimentum lobortis. Nunc rhoncus lobortis nibh sit amet venenatis. Fusce eget cursus tellus. Proin molestie dolor eget nisl faucibus iaculis vel imperdiet tellus. Integer vel ligula sit amet lacus ornare facilisis vel et ipsum. Ut laoreet ipsum purus, sit amet posuere sapien eleifend et. In non felis a libero tempor vestibulum.",
                 "price": 19.99,
                 "currency": "EUR",
                 "status": "active",
