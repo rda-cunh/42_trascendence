0cca743a97b7eb0e2114a2109243b3b3c144fa8c
rda-cunh <ricard.mendes@gmail.com>
2026-04-21 01:35:28 +0100

    Revert "Merge branch 'main' into rda-cunh_auth"
    This reverts commit c3f394fb5152eda3001bd00f6d397355808f4d3b, reversing
    changes made to 60c69db6044cab09ebb481c946241935ecf675dd.

--- srcs/pki/scripts/gen-certs.sh
+++ srcs/pki/scripts/gen-certs.sh
@@ -64,7 +64,6 @@ rm -f "$INT_CSR" "$SAN_CONF" "$CERTS_DIR"/*.srl
 
 echo "Setting standard file permissions..."
 chmod 644 "$CERTS_DIR"/*.crt
-chmod 644 "$INT_KEY"
-chmod 600 "$CA_KEY"
+chmod 600 "$CERTS_DIR"/*.key
 
 echo "PKI Initialization Complete."
