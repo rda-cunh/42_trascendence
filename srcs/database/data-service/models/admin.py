bc4b27b3cb5837cfac0a6889b4fb51de91c575f0
Leonardo Maes <devsleo@hotmail.com>
2026-05-23 20:23:55 +0100

    feat: admin dashboard; endpoint to view all images being used

--- srcs/database/data-service/models/admin.py
+++ srcs/database/data-service/models/admin.py
@@ -12,4 +12,13 @@ class UserInfo(BaseModel):
 	phone:		Optional[str] = None
 	created_at:	datetime
 	updated_at:	datetime
-	
+
+class DashboardInfo(BaseModel):
+	total_revenue:		int
+	total_users:		int
+	total_orders:		int
+	active_listings:	int
+	orders_trend:		list[int]
+	revenue_overview:	list[int]
+	months:				list[str]
+
